e26611868600b379115c663b56e139599d4b7450
Dániel <nagydaniel04@yahoo.com>
2016-05-24 15:38:09 +0300

    save  the data in database

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
     <head> 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
         <script>
-        $(document).ready(function(){
-             $("#country").change(function(){
-                $.ajax({
-                    url:"find_counties.php",
-                    method:"POST",
-                    data: {id : $("#country").val()}
-                }).success(function(result){
-                    $("#county").html(result);             
+            $(document).ready(function () {
+                $("#country").change(function () {
+                    $.ajax({
+                        url: "find_counties.php",
+                        method: "POST",
+                        data: {id: $("#country").val()}
+                    }).success(function (result) {
+                        $("#county").html(result);
+                    });
                 });
             });
-        });    
         </script>
         <style>
             .wrap{
@@ -34,59 +34,59 @@
         </style>
     </head>
     <body>
-        <?php   
-        if(isset($oksucces)){
-            include 'save.php';          
+        <?php
+        include 'save.php';
+        if (isset($oksucces)) {
             echo "Succes";
         }
         ?>
-        <form method="POST" action="save.php" class="form"> 
-        <div class="wrap">
-            <div class="left">
-                <fieldset class="form-group">
-                    <label> Image: </label>
-                    <input type="file" name="image" name="" id="camerainput1" accept="image/*" capture="camera"><br>
-                </fieldset>
+        <form method="POST" action="index.php" class="form"> 
+            <div class="wrap">
+                <div class="left">
+                    <fieldset class="form-group">
+                        <label> Image: </label>
+                        <input type="file" name="image" name="" id="camerainput1" accept="image/*" capture="camera"><br>
+                    </fieldset>
+                </div>
+                <div class="rigth">
+                    <fieldset class="form-group">
+                        <label> Name: </label>
+                        <?php if (true): ?>
+                         <input type="text" value="<?php if (isset($name)) echo $name; ?>" name="name" id="name" ><br>
+                        <?php endif; ?>
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <label> Email: </label>
+                        <?php if (true): ?>
+                            <input type="text" name="email" value="<?php if (isset($name)) echo $name; ?>" id="email" ><br>
+                        <?php endif; ?>
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <label> Country: </label>
+                        <select name="country" id="country">
+                            <option value="default">Choose a Country...</option>
+                            <?php include 'find_countries.php'; ?>
+                        </select><br>
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <label> County: </label>
+                        <select id="county" name="county">
+                            <option value="default">Choose a County</option>
+                        </select><br>
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <label> Password: </label>
+                        <input type="password" name="password"><br>
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <label> Password again: </label>                  
+                        <input type="password" name="repassword" ><br>
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <input class="btn" type="submit" name="submit" value="Submit"><br>
+                    </fieldset>
+                </div>
             </div>
-            <div class="rigth">
-                <fieldset class="form-group">
-                    <label> Name: </label>
-                    <?php if(true): ?>
-                    <input type="text" name="<?php if(isset($name))echo $name; ?>" id="name" ><br>
-                    <?php endif; ?>
-                </fieldset>
-                <fieldset class="form-group">
-                    <label> Email: </label>
-                    <?php if(true): ?>
-                    <input type="text" name="<?php if(isset($name))echo $name; ?>" id="email" ><br>
-                    <?php endif; ?>
-                </fieldset>
-                <fieldset class="form-group">
-                    <label> Country: </label>
-                    <select name="country" id="country">
-                        <option value="default">Choose a Country...</option>
-                        <?php include 'find_countries.php'; ?>
-                    </select><br>
-                </fieldset>
-                <fieldset class="form-group">
-                     <label> County: </label>
-                    <select id="county">
-                        <option value="default">Choose a County</option>
-                    </select><br>
-                </fieldset>
-                <fieldset class="form-group">
-                    <label> Password: </label>
-                    <input type="password" ><br>
-                </fieldset>
-                <fieldset class="form-group">
-                    <label> Password again: </label>                  
-                    <input type="password" ><br>
-                </fieldset>
-                <fieldset class="form-group">
-                    <input type="button" value="Submit" name="submit" class="submit"><br>
-                </fieldset>
-            </div>
-        </div>
         </form>
     </body>
 </html>
--- save.php
+++ save.php
@@ -1,13 +1,22 @@
 <?php
-        $servername="localhost";
-        $user="root";
-        $passw="";
-        $dbname="user";
-        
-        $conn=mysqli_connect($servername, $user, $passw, $dbname);
-        //echo $_POST["name"];
-        if (!$conn){
-            die("connection failed:".mysqli_connect_error());
-        }
-        else echo "csatlakozott"
-?>
+$servername = "localhost";
+$user = "root";
+$passw = "";
+$dbname = "user";
+$conn = mysqli_connect($servername, $user, $passw, $dbname);
+if (!$conn) {
+    die("connection failed:" . mysqli_connect_error());
+}
+$name=$_POST["name"];
+$email=$_POST["email"];
+$country_id=$_POST["country"];
+$county_id=$_POST["county"];
+$password=$_POST["password"];
+$insert="INSERT INTO users(name,email,country_id,county_id,password)"
+        . "VALUES ('$name','$email','$country_id','$county_id','$password')";
+$sql=mysqli_query($conn, $insert);
+if($sql){
+    $oksucces=1;
+}
+
+
